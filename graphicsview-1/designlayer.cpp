42fcfb5fc979bbeb82ea7221e695c214b4bfb820
Christian Gagneraud <chgans@gna.org>
2015-04-09 17:18:31 +1200

    graphicsview-1: Remove useless updating calls since DesignLayer is flagged as ItemHasNoContent

--- graphicsview-1/designlayer.cpp
+++ graphicsview-1/designlayer.cpp
@@ -36,9 +36,7 @@ void DesignLayer::setColor(const QColor &color)
     if (m_color == color)
         return;
 
-    prepareGeometryChange();
     m_color = color;
-    update();
 
     emit colorChanged(m_color);
 }
